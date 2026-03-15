4c2965117d97608a3cb5f90f1ee77d4914e47703
RubinTropfen007 <ebeling557@gmail.com>
2026-03-15 14:06:23 +0100

    Complete Rewrite of the Original Engine. Support is now currently only for Linux/Unix Systems until i feel like working with the Windows API... This Game can now work with shared Libraries as long as specific structs and functions are exposed. I will add Examples later on.

--- include/beg.h
+++
@@ -1,7 +0,0 @@
-#ifndef BEG_H
-#define BEG_H
-
-long long Beg(int64_t *konto);
-
-#endif // BEG_H
-
--- include/card.h
+++ include/card.h
@@ -1,7 +1,10 @@
-#ifndef CARD_H
-#define CARD_H
-typedef struct {
-    char name[32];
-    int value;
-} PlayingCard;
-#endif // CARD_H
+// Literally just a header file for the Card struct and related functions.
+#ifndef CASINO_CARD_H
+#define CASINO_CARD_H
+#include <stdint.h>
+
+typedef struct Card {
+    char name[64]; // e.g. "Ace of Spades"
+    uint8_t value; // Blackjack value (2-10 for number cards, 10 for face cards, 11 for Ace)
+} Card;
+#endif
